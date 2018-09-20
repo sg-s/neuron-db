75ebaefb5f2d9f9ee63ffe8368c3c6abc42d93bb
gitlab@srinivas.gs <code@srinivas.gs>
2018-09-20 18:35:02 -0400

    many changes, using the new Data class

--- @neuroDB/neuroDB.m
+++ @neuroDB/neuroDB.m
@@ -17,12 +17,13 @@ properties
 
 	prefix = 'prinz/';
 
-	metrics
-	all_g
+	results@Data
 
 	workers
 	current_pool
 	num_workers
+
+	sim_chunk_size = 1e3
 
 end
 
@@ -31,22 +32,21 @@ methods
 	% constructor
 	function self = neuroDB()
 
+		results = new(Data(xtools.V2metrics(zeros(1e4,1))));
+		results.add('all_g',8);
+		self.results = results;
+
 		% make xolotl object 
 		A = 0.0628;
 		channels = {'NaV','CaT','CaS','ACurrent','KCa','Kd','HCurrent'};
 		E =         [50   30  30 -80 -80 -80   -20];
-
 		x = xolotl;
 		x.add('compartment','AB','Cm',10,'A',A);
-
-
 		% add Calcium mechanism
 		x.AB.add('CalciumMech1');
-
 		for i = 1:length(channels)
 			x.AB.add([self.prefix channels{i}],'gbar',rand*10,'E',E(i));
 		end
-
 		x.AB.add('Leak','gbar',0);
 
 		x.t_end = 20e3;
@@ -177,91 +177,7 @@ methods
 	end
 
 
-	function simulate(self)
 
-
-		x = self.x;
-
-		n_sims = 0;
-		disp('A        CaS       CaT       H        KCa         Kd        Leak      NaV       FR (Hz)    Speed')
-		disp('-------------------------------------------------------------------------------------------------------')
-
-		while true
-
-			if rem(n_sims,1000) == 0 
-				temp = x.integrate;
-				metrics = repmat(xtools.V2metrics(0*temp),1000,1);
-				all_g = NaN(1000,8);
-				idx =  1;
-				time_idx = 1;
-				tic
-			end
-
-			% pick a random point in the cube
-			x.AB.NaV.gbar = rand*diff(self.NaV_range) + self.NaV_range(1);
-			x.AB.CaT.gbar = rand*diff(self.CaT_range) + self.CaT_range(1);
-			x.AB.CaS.gbar = rand*diff(self.CaS_range) + self.CaS_range(1);
-			x.AB.ACurrent.gbar = rand*diff(self.A_range) + self.A_range(1);
-			x.AB.KCa.gbar = rand*diff(self.KCa_range) + self.KCa_range(1);
-			x.AB.Kd.gbar = rand*diff(self.Kd_range) + self.Kd_range(1);
-			x.AB.HCurrent.gbar = rand*diff(self.H_range) + self.H_range(1);
-			x.AB.Leak.gbar = rand*diff(self.Leak_range) + self.Leak_range(1);
-
-			% transient
-			x.reset;
-			x.AB.CaS.E = 30;
-			x.AB.CaT.E = 30;
-			x.integrate;
-
-			V = x.integrate;
-
-			time_idx = time_idx + 1;
-
-			t = toc;
-
-
-			try
-				metrics(idx) = xtools.V2metrics(V,'sampling_rate',1/x.dt);
-			catch
-				disp('Something went wrong with trying to measure the metrics')
-				continue
-			end
-
-
-			if metrics(idx).firing_rate == 0
-				disp('Silent neuron, skipping...')
-				continue
-			end
-
-			all_g(idx,:) = x.get('*gbar');
-
-			% show this live 
-			for i = 1:8
-				fprintf(flstring(oval(all_g(idx,i)),10))
-			end
-
-			fprintf(flstring(oval(metrics(idx).firing_rate),10))
-			fprintf(flstring(oval(time_idx/t),10))
-			fprintf('\n')
-
-
-			n_sims = n_sims + 1;
-			idx = idx + 1;
-
-			if idx > 1000
-				% need to save 
-				disp('Saving...')
-				save_name = [GetMD5(now) '.neuroDB'];
-				save(save_name,'all_g','metrics','-v7.3');
-
-				disp('A        CaS       CaT       H        KCa         Kd        Leak      NaV       FR (Hz)    Speed')
-				disp('-------------------------------------------------------------------------------------------------------')
-
-			end
-
-		end
-
-	end
 
 
 
---
+++ @neuroDB/simulate.m
@@ -0,0 +1,90 @@
+	function simulate(self)
+
+
+		x = self.x;
+
+		n_sims = 0;
+		disp('A        CaS       CaT       H        KCa         Kd        Leak      NaV       FR (Hz)    Speed')
+		disp('-------------------------------------------------------------------------------------------------------')
+
+		results = Data(xtools.V2metrics(zeros(1e4,1)));
+		results = results.new();
+		results.add('all_g',8);
+		results.prealloc(self.sim_chunk_size);
+
+		while true
+
+			if rem(n_sims,self.sim_chunk_size) == 0 
+				time_idx = 1;
+				tic
+			end
+
+			% pick a random point in the cube
+			x.AB.NaV.gbar = rand*diff(self.NaV_range) + self.NaV_range(1);
+			x.AB.CaT.gbar = rand*diff(self.CaT_range) + self.CaT_range(1);
+			x.AB.CaS.gbar = rand*diff(self.CaS_range) + self.CaS_range(1);
+			x.AB.ACurrent.gbar = rand*diff(self.A_range) + self.A_range(1);
+			x.AB.KCa.gbar = rand*diff(self.KCa_range) + self.KCa_range(1);
+			x.AB.Kd.gbar = rand*diff(self.Kd_range) + self.Kd_range(1);
+			x.AB.HCurrent.gbar = rand*diff(self.H_range) + self.H_range(1);
+			x.AB.Leak.gbar = rand*diff(self.Leak_range) + self.Leak_range(1);
+
+			% transient
+			x.reset;
+			x.AB.CaS.E = 30;
+			x.AB.CaT.E = 30;
+			x.integrate;
+
+			V = x.integrate;
+
+			time_idx = time_idx + 1;
+
+			t = toc;
+
+
+			try
+				new_metrics = xtools.V2metrics(V,'sampling_rate',1/x.dt);
+			catch
+				disp('Something went wrong with trying to measure the metrics')
+				continue
+			end
+
+
+			if new_metrics.firing_rate == 0
+				disp('Silent neuron, skipping...')
+				continue
+			end
+
+			this_all_g = x.get('*gbar');
+
+			% show this live 
+			for i = 1:8
+				fprintf(flstring(oval(this_all_g(i)),10))
+			end
+
+			fprintf(flstring(oval(new_metrics.firing_rate),10))
+			fprintf(flstring(oval(time_idx/t),10))
+			fprintf('\n')
+
+
+			% append
+			new_metrics.all_g = this_all_g;
+			results+new_metrics;
+
+			n_sims = n_sims + 1;
+
+			if results.size == self.sim_chunk_size
+				% need to save 
+				disp('Saving...')
+				save_name = [GetMD5(now) '.neuroDB'];
+				save(save_name,'results','-v7.3');
+
+				results.reset;
+
+				disp('A        CaS       CaT       H        KCa         Kd        Leak      NaV       FR (Hz)    Speed')
+				disp('-------------------------------------------------------------------------------------------------------')
+
+			end
+
+		end
+
